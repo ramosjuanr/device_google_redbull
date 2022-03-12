cbace5c38265b8d5691fe0ec8ec0121e3dcc75e7
Juan Ramos <jramos69@gmail.com>
2022-03-12 17:32:04 -0500

    Add vendor.init code
    Change-Id: I99d084ae436f3c2a64031c91ad8d116ba221a739

--- soong/pixel_soong_config.mk
+++ soong/pixel_soong_config.mk
@@ -7,3 +7,11 @@ SOONG_CONFIG_sensor += \
         enable_sensor_ssc_for_soong
 
 SOONG_CONFIG_sensor_enable_sensor_ssc_for_soong := $(ENABLE_SENSOR_SSC_FOR_SOONG)
+
+SOONG_CONFIG_NAMESPACES += lineageGlobalVars
+SOONG_CONFIG_lineageGlobalVars += \
+	target_init_vendor_lib
+
+TARGET_INIT_VENDOR_LIB ?= vendor_init
+
+SOONG_CONFIG_lineageGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
